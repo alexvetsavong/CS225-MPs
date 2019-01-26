33823b74b6cd7eaec50a261ec95d041ce1af9739
avetsa2 <avetsa2@illinois.edu>
2019-01-26 16:37:14 -0600

    Completed part 1 and 2 of mp, and now working on the third part. Not creative tho, so let's see how it goes.

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -1,16 +1,50 @@
 #include "cs225/PNG.h"
 #include "cs225/HSLAPixel.h"
+#include <cmath>
+using namespace cs225;
 
 #include <string>
 
+PNG* setupOutput(unsigned w, unsigned h){
+  PNG* image = new PNG(w,h);
+  return image;
+}
 
 void rotate(std::string inputFile, std::string outputFile) {
   // TODO: Part 2
+  PNG* original = new PNG;
+  original->readFromFile(inputFile);
+  unsigned int width = original->width();
+  unsigned int height = original->height();
+  PNG* output = setupOutput(width, height);
+
+  for (unsigned int y = 0; y < height; y++) {
+    for (unsigned int x = 0; x < width; x++) {
+      HSLAPixel& currInPixel = original->getPixel(x,y);
+      HSLAPixel& currOutPixel = output->getPixel(width-1-x, height-1-y);
+      currOutPixel = currInPixel;
+    }
+  }
+
+  output->writeToFile(outputFile);
+  delete output;
+  delete original;
 }
 
-cs225::PNG myArt(unsigned int width, unsigned int height) {
-  cs225::PNG png(width, height);
+PNG myArt(unsigned int width, unsigned int height) {
+  PNG png = PNG(width,height);
   // TODO: Part 3
 
+  for (unsigned int y = 0; y < height; y++) {
+    for (unsigned int x = 0; x < width; x++) {
+      HSLAPixel& outPixel = png.getPixel(x,y);
+      if(pow((double)x - (double)width/2.0, 2.0) + pow((double)y - (double)height/2.0, 2.0) <= (width/2)) {
+        outPixel.h = pow((double)x,3) + pow((double)y,2) - 36.0;
+      }
+      else {
+        outPixel.h = (double)width/360.0 * x;
+      }
+    }
+  }
   return png;
 }
