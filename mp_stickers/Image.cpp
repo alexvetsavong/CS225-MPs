d7bb30d1d09bf9dd4b9a4832b41a5ecc9354c3c0
avetsa2 <avetsa2@illinois.edu>
2019-02-04 21:54:26 -0600

    Completed part 1 of MP_stickers

---
+++ mp_stickers/Image.cpp
@@ -0,0 +1,189 @@
+#include "cs225/PNG.h"
+#include "Image.h"
+#include <cmath>
+
+using namespace cs225;
+
+double hueWrapAround(double hue){
+  return hue - 360*floor(hue/360);
+}
+
+void Image::lighten(){
+  for(unsigned y = 0; y < height(); y++){
+    for(unsigned x = 0; x < width(); x++){
+      HSLAPixel & currPixel = getPixel(x,y);
+      if (currPixel.l + 0.1 <= 1 && currPixel.l + 0.1 >= 0){
+        currPixel.l += 0.1;
+      }
+      else if (currPixel.l + 0.1 > 1) currPixel.l = 1;
+      else if (currPixel.l + 0.1 < 0) currPixel.l = 0;
+    }
+  }
+}
+
+void Image::lighten(double amount){
+  for(unsigned y = 0; y < height(); y++){
+    for(unsigned x = 0; x < width(); x++){
+      HSLAPixel & currPixel = getPixel(x,y);
+      if (currPixel.l + amount <= 1 && currPixel.l + amount >= 0){
+        currPixel.l += amount;
+      }
+      else if (currPixel.l + amount > 1) currPixel.l = 1;
+      else if (currPixel.l + amount < 0) currPixel.l = 0;
+    }
+  }
+}
+
+void Image::darken(){
+  for(unsigned y = 0; y < height(); y++){
+    for(unsigned x = 0; x < width(); x++){
+      HSLAPixel & currPixel = getPixel(x,y);
+      if (currPixel.l - 0.1 <= 1 && currPixel.l - 0.1 >= 0){
+        currPixel.l -= 0.1;
+      }
+      else if (currPixel.l - 0.1 > 1) currPixel.l = 1;
+      else if (currPixel.l - 0.1 < 0) currPixel.l = 0;
+    }
+  }
+}
+
+void Image::darken(double amount){
+  for(unsigned y = 0; y < height(); y++){
+    for(unsigned x = 0; x < width(); x++){
+      HSLAPixel & currPixel = getPixel(x,y);
+      if (currPixel.l - amount < 1 && currPixel.l - amount > 0){
+        currPixel.l -= amount;
+      }
+      else if (currPixel.l - amount > 1) currPixel.l = 1;
+      else if (currPixel.l - amount < 0) currPixel.l = 0;
+    }
+  }
+}
+
+void Image::saturate(){
+  for(unsigned y = 0; y < height(); y++){
+    for(unsigned x = 0; x < width(); x++){
+      HSLAPixel & currPixel = getPixel(x,y);
+      if (currPixel.s + 0.1 < 1 && currPixel.s + 0.1 > 0){
+        currPixel.s += 0.1;
+      }
+      else if (currPixel.s + 0.1 > 1) currPixel.s = 1;
+      else if (currPixel.s + 0.1 < 0) currPixel.s = 0;
+    }
+  }
+}
+
+void Image::saturate(double amount){
+  for(unsigned y = 0; y < height(); y++){
+    for(unsigned x = 0; x < width(); x++){
+      HSLAPixel & currPixel = getPixel(x,y);
+      if (currPixel.s + amount < 1 && currPixel.s + amount > 0){
+        currPixel.s += amount;
+      }
+      else if (currPixel.s + amount > 1) currPixel.s = 1;
+      else if (currPixel.s + amount < 0) currPixel.s = 0;
+    }
+  }
+}
+
+void Image::desaturate(){
+  for(unsigned y = 0; y < height(); y++){
+    for(unsigned x = 0; x < width(); x++){
+      HSLAPixel & currPixel = getPixel(x,y);
+      if (currPixel.s - 0.1 < 1 && currPixel.s - 0.1 > 0){
+        currPixel.s -= 0.1;
+      }
+      else if (currPixel.s - 0.1 > 1) currPixel.s = 1;
+      else if (currPixel.s - 0.1 < 0) currPixel.s = 0;
+    }
+  }
+}
+
+void Image::desaturate(double amount){
+  for(unsigned y = 0; y < height(); y++){
+    for(unsigned x = 0; x < width(); x++){
+      HSLAPixel & currPixel = getPixel(x,y);
+      if (currPixel.s - amount < 1 && currPixel.s - amount > 0){
+        currPixel.s -= amount;
+      }
+      else if (currPixel.s - amount > 1) currPixel.s = 1;
+      else if (currPixel.s - amount < 0) currPixel.s = 0;
+    }
+  }
+}
+
+void Image::grayscale(){
+  for(unsigned y = 0; y < height(); y++){
+    for(unsigned x = 0; x < width(); x++){
+      HSLAPixel & currPixel = getPixel(x,y);
+      currPixel.h = 0;
+    }
+  }
+}
+
+void Image::rotateColor(double degrees){
+  for(unsigned y = 0; y < height(); y++){
+    for(unsigned x = 0; x < width(); x++){
+      HSLAPixel & currPixel = getPixel(x,y);
+      currPixel.h = hueWrapAround(currPixel.h + degrees);
+    }
+  }
+}
+
+void Image::illinify(){
+  for(unsigned y = 0; y < height(); y++){
+    for(unsigned x = 0; x < width(); x++){
+      HSLAPixel & currPixel = getPixel(x, y);
+      if(currPixel.h <= (113.5) || currPixel.h >= (293.5)){
+        currPixel.h = 11.0;
+      }
+      else currPixel.h = 216.0;
+    }
+  }
+}
+
+void Image::scale(double factor){
+  Image temp;
+  temp.resize(factor*width(),factor*height());
+  for(unsigned y = 0; y < height(); y++){
+    for(unsigned x = 0; x < width(); x++){
+      HSLAPixel & currPixel = getPixel(x,y);  // gets the pixel data of the original image at current x,y
+      for(unsigned j = 0; j < factor; j++){
+        for(unsigned i = 0; i < factor; i++){
+          HSLAPixel & outPixel = temp.getPixel(x*factor + i, y*factor + j);
+          outPixel = currPixel;
+        }
+      }
+    }
+  }
+  *this = temp;
+}
+
+void Image::scale(unsigned w, unsigned h){  //e.g 800x20
+  double factor = 0.0;
+  double aspectRatio = width()/height();
+
+  if(w>h){
+    if (w/(w/aspectRatio) == aspectRatio && (w/aspectRatio) <= h){
+      factor = w/width();
+    }
+  }
+  else if ((h*aspectRatio)/h == aspectRatio && (h*aspectRatio <= w)){
+    factor = h/height();
+  }
+
+  Image temp;
+  temp.resize(factor*width(),factor*height());
+  for(unsigned y = 0; y < height(); y++){
+    for(unsigned x = 0; x < width(); x++){
+      HSLAPixel & currPixel = getPixel(x,y);  // gets the pixel data of the original image at current x,y
+      for(unsigned j = 0; j < factor; j++){
+        for(unsigned i = 0; i < factor; i++){
+          HSLAPixel & outPixel = temp.getPixel(x*factor + i, y*factor + j);
+          outPixel = currPixel;
+        }
+      }
+    }
+  }
+  *this = temp;
+}
