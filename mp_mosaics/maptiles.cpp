7eba1a3493ca6d71d29d4d4aaff7bd04c26fff38
avetsa2 <avetsa2@illinois.edu>
2019-04-02 22:59:29 -0500

    Debugging MP5.2 for segfaults in basic test case.

--- mp_mosaics/maptiles.cpp
+++ mp_mosaics/maptiles.cpp
@@ -21,7 +21,37 @@ MosaicCanvas* mapTiles(SourceImage const& theSource,
     /**
      * @todo Implement this function!
      */
+    size_t rows = theSource.getRows();
+    size_t cols = theSource.getColumns();
+    MosaicCanvas* newCanvas = new MosaicCanvas(rows, cols);
 
-    return NULL;
+    LUVAPixel averageColor = LUVAPixel();
+    Point<3> tileAsPoint = Point<3>();
+    vector<Point<3>> tileColors;
+
+    TileImage writeImage = TileImage();
+
+    std::map<Point<3>, TileImage> pointToTile;
+
+    for(size_t i = 0; i < theTiles.size(); i++){
+      averageColor = theTiles[i].getAverageColor();
+      tileAsPoint = convertToXYZ(averageColor);
+      tileColors.push_back(tileAsPoint);
+      pointToTile.insert(std::pair<Point<3>, TileImage>(tileAsPoint, theTiles[i]));
+    }
+
+    KDTree<3> tiles(tileColors);
+
+    for (size_t j = 0; j < cols; j++){
+      for (size_t k = 0; k < rows; k++){
+        averageColor = theSource.getRegionColor(k,j);
+        tileAsPoint = convertToXYZ(averageColor);
+        tileAsPoint = tiles.findNearestNeighbor(tileAsPoint);
+
+        writeImage = pointToTile[tileAsPoint];
+        newCanvas->setTile(k,j, &writeImage);
+      }
+    }
+
+    return newCanvas;
 }
-
