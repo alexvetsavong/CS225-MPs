e348c3a40e455c0c3d62601f6e1f1634b5ab4f62
avetsa2 <avetsa2@illinois.edu>
2019-04-13 05:40:33 -0500

    Implemented dsets for part 1a for the MP.

--- mp_mazes/dsets.cpp
+++ mp_mazes/dsets.cpp
@@ -1,1 +1,53 @@
 /* Your code here! */
+#include "dsets.h"
+
+DisjointSets::DisjointSets(){
+/* initialize an empty vector */
+  // _elems.clear();
+}
+
+void DisjointSets::addelements(int num){
+  size_t current_size = _elems.size();
+  for (size_t i = current_size; i < current_size + num; i++){
+    _elems.push_back(i);
+  }
+}
+
+int DisjointSets::find (int elem){
+
+  while(_elems[elem] != elem){
+    _elems[elem] = _elems[_elems[elem]];
+    elem = _elems[elem];
+  }
+
+  return elem;
+}
+
+void DisjointSets::setunion (int a, int b){
+  int root_a = find(a);
+  int root_b = find(b);
+
+  if (size(root_a) < size(root_b)) {
+    _elems[root_a] = _elems[root_b];
+  }
+
+  else {
+    _elems[root_b] = _elems[root_a];
+  }
+
+}
+
+int DisjointSets::size (int elem){
+  int size = 0;
+  int root = find(elem);
+
+  std::vector<int>::iterator it;
+
+  for(it = _elems.begin(); it != _elems.end(); it++){
+    if (find(*it) == root){
+      size++;
+    }
+  }
+
+  return size;
+}
--- mp_mazes/dsets.h
+++ mp_mazes/dsets.h
@@ -1,1 +1,21 @@
 /* Your code here! */
+#pragma once
+#include <vector>
+#include <algorithm>
+
+class DisjointSets {
+public:
+  DisjointSets();
+
+  /* since using STL vector, compiler-default big 3
+     is sufficient */
+
+  void addelements (int num);
+  int find (int elem);
+  void setunion (int a, int b);
+  int size (int elem);
+
+private:
+  std::vector<int> _elems;
+  std::vector<int> _size;
+};
