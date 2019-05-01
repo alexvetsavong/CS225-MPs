142bdba624433b4260809452f0752c9cf9364c05
avetsa2 <avetsa2@illinois.edu>
2019-04-30 19:44:14 -0500

    implemented everything other than remove* functions.

--- mp_stories/Graph.hpp
+++ mp_stories/Graph.hpp
@@ -23,7 +23,23 @@ unsigned int Graph<V,E>::numVertices() const {
 template <class V, class E>
 unsigned int Graph<V,E>::degree(const V & v) const {
   // TODO: Part 2
-  return adjList.at(v.key()).size();
+
+  // dummy edge for checking if graph is directed or not
+  E e(v,v);
+
+  // used to keep track of how many edges have v as a destination
+  unsigned int degree = 0;
+
+  // if graph is directed, keep track of how many edges end with v
+  if (e.directed()){
+    for (E_byRef x : edgeList) {
+      if (x.get().dest() == v) degree++;
+    }
+  }
+
+  if (adjList.find(v.key()) != adjList.end())   // check if vertex is a source of any edges
+    return degree + adjList.at(v.key()).size(); // if it is, return in-degree + out-degree;
+  else return degree; // otherwise return only the in-degree
 }
 
 
@@ -48,6 +64,8 @@ V & Graph<V,E>::insertVertex(std::string key) {
 template <class V, class E>
 void Graph<V,E>::removeVertex(const std::string & key) {
   // TODO: Part 2
+  adjList.erase(key);
+  vertexMap.erase(key);
 }
 
 
@@ -67,16 +85,19 @@ E & Graph<V,E>::insertEdge(const V & v1, const V & v2) {
   typename std::unordered_map<std::string, std::list<edgeListIter>>::iterator
     it = adjList.find(v1.key());
 
+  // if the key is not in the adjacency list, create a new pair and place it in there
   if (it == adjList.end()) {
     std::list<edgeListIter> incidentEdges;
     incidentEdges.push_front(edgeList.begin());
     adjList.emplace(v1.key(), incidentEdges);
   }
 
+  // otherwise, just push the new edge into the list associated with vertex
   else {
     it->second.push_front(edgeList.begin());
   }
 
+  // if the edge is undirected, do the same as above with swapped endpoints
   if (!e.directed()) {
     it = adjList.find(v2.key());
 
@@ -104,7 +125,6 @@ E & Graph<V,E>::insertEdge(const V & v1, const V & v2) {
 template <class V, class E>
 void Graph<V,E>::removeEdge(const std::string key1, const std::string key2) {
   // TODO: Part 2
-
 }
 
 
@@ -129,6 +149,22 @@ template <class V, class E>
 const std::list<std::reference_wrapper<E>> Graph<V,E>::incidentEdges(const std::string key) const {
   // TODO: Part 2
   std::list<E_byRef> edges;
+
+  E dummy = E(V(key),V(key));
+
+  if (adjList.find(key) != adjList.end()) {
+    for (edgeListIter it : adjList.at(key)) {
+      edges.push_front(*it);
+    }
+  }
+
+  if (dummy.directed()) {
+    for (E_byRef e : edgeList) {
+      if (e.get().dest().key() == key)
+        edges.push_front(std::ref(e));
+    }
+  }
+
   return edges;
 }
 
@@ -143,5 +179,33 @@ const std::list<std::reference_wrapper<E>> Graph<V,E>::incidentEdges(const std::
 template <class V, class E>
 bool Graph<V,E>::isAdjacent(const std::string key1, const std::string key2) const {
   // TODO: Part 2
+  V v1 = V(key1);
+  V v2 = V(key2);
+  E dummy = E(V(key1),V(key1));
+
+  if (!dummy.directed()) {
+    if (degree(v1) <= degree(v2) && adjList.find(key1) != adjList.end()) {
+      for (edgeListIter e : adjList.at(key1)) {
+        if (e->get().source().key() == key2 || e->get().dest().key() == key2)
+          return true;
+      }
+    }
+    else {
+      for (edgeListIter e : adjList.at(key2)) {
+        if (e->get().source().key() == key1 || e->get().dest().key() == key1)
+          return true;
+      }
+    }
+  }
+
+  else {
+    if (adjList.find(key1) != adjList.end()) {
+      for (edgeListIter e : adjList.at(key1)) {
+        if (e->get().source().key() == key2 || e->get().dest().key() == key2)
+        return true;
+      }
+    }
+  }
+
   return false;
 }
